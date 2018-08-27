30b27125197c5b6748219e3f35c025732bc761d3
Vivek Ravikumar <vr329@njit.edu>
2018-08-26 18:09:51 -0700

    Working on Sum

--- index-spec.js
+++ index-spec.js
@@ -45,13 +45,104 @@ describe("The test of an E-Commerce Application", () => {
     emptyValues("v");
   });
 
-  fit("Clicks on Shop Link", () => {
+  const selectItem = product => {
+    element.all(by.tagName("app-card")).each(item => {
+      item
+        .element(by.css("h4[class='card-title'] a"))
+        .getText()
+        .then(text => {
+          if (text === product) {
+            item.element(by.css("button[class*='btn-info']")).click();
+          }
+        });
+    });
+  };
+
+  fit("Clicks on Shop Link", done => {
     fillTheForm("Vivek", "vr329@njit.edu", "password", "Female", 0);
     // emptyValues(email, " ");
     element(by.linkText("Shop")).click();
     //Take all cards into list and iterate thru them and get the title if title = desired title
     //Then select Add button
+    selectItem("Samsung Note 8");
+    selectItem("iphone X");
+    selectItem("Nokia Edge");
 
-    element.all(by.tagName("app-card"));
+    element(by.partialLinkText("Checkout"))
+      .getText()
+      .then(text => {
+        // console.log(text);
+        const res = text.split("(");
+        console.log(res);
+        // expect(parseInt(res[1].trim().charAt(0))).toBe(3);
+      });
+    //nav-link btn btn-primary
+
+    element(by.css("a[class='nav-link btn btn-primary']")).click();
+    let arr = [];
+
+    // element(by.tagName("tbody"))
+    //   .all(by.tagName("tr"))
+    //   .each(item => {
+    //     item
+    //       .element(by.css("td:nth-child(4)"))
+    //       .getText()
+    //       .then((val, i) => {
+    //         if (parseInt(val)) {
+    //           arr.unshift(val);
+    //           console.log(arr);
+    //         }
+    //         done();
+    //       });
+    //   });
+
+    element(by.tagName("tbody"))
+      .all(by.tagName("tr"))
+      .each(item => item.element(by.css("td:nth-child(4)")))
+
+      .getText()
+      .then(val => console.log(val));
+
+    // console.log(abcd);
+    // console.log(typeof val);
+    // var res = val.split(". ");
+
+    // element.all(by.tagName("tr")).each(item => {
+    //   item.element
+    //     .all(by.css("td:nth-child(3)"))
+    //     .element(by.tagName("strong"))
+    //     .getText()
+    //     .then(val => console.log(val));
+    // });
+
+    // element(by.tagName("tbody")).then(() => {
+    //   element
+    //     .all(by.tagName("tr"))
+    //     .count()
+    //     .then(val => console.log(val));
+    // });
+
+    // element
+    //   .all(by.tagName("tr"))
+    //   .get(4)
+    //   .getText()
+    //   .then(val => {
+    //     const res = val;
+    //   });
+
+    // .each(item => {
+    // item.element(by.css("text-center"));
+    // .element(by.tagName("strong"))
+    // .getText()
+    // .then(val => console.log(val));
   });
+
+  // .element(by.css("td:nth-child(3)"))
+  // .element(by.tagName("strong"))
+  // .getText()
+  //   .getText()
+  //   .then(val => console.log(val));
+  // item.element(by.css("td:nth-child(3)"));
+  // });
+  // });
 });
